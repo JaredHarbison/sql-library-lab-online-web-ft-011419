2fda7834fdfb97bcf496b1d87e7690993189d6a9
Jared Harbison <jared.harbison@gmail.com>
2019-02-19 17:50:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM(
-      SELECT series.title AS title, COUNT(characters.species), )
+      SELECT series.title AS title, COUNT(characters.species_id) AS  )
 end
 
 def select_character_names_and_number_of_books_they_are_in
