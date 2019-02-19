bcc38512d4f79baedd3c62ceaec4ea16e01f7f58
Jared Harbison <jared.harbison@gmail.com>
2019-02-19 17:45:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,18 +1,29 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM books
+  WHERE books.series_id = 1 
+  ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto FROM characters
+  ORDER BY length(characters.motto) DESC
+  LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, count(characters.species) AS species_count FROM characters
+  GROUP BY characters.species 
+  ORDER BY species_count DESC
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name AS author_name, subgenres.name AS subgenre_name FROM series
+    INNER JOIN authors
+      ON series.author_id = authors.id
+    INNER JOIN subgenres
+      ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
